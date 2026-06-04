9481832153cffc58034e96190a97b8da1771064d
KC <kochihuang202@github.com>
2026-06-05 00:49:31 +0800

    chore: bump deploy version to invalidate CDN cache

--- cloudflare/worker-proxy.js
+++ cloudflare/worker-proxy.js
@@ -24,7 +24,7 @@ function resolveAssetPath(pathname) {
   return pathname;
 }
 
-const DEPLOY_VERSION = "v_2026_06_04_21_30";
+const DEPLOY_VERSION = "v_2026_06_05_00_50";
 
 async function fetchAsset(pathname) {
   const assetPath = resolveAssetPath(pathname);
