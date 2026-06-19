7c6dac685b9757d511a13beddad08277b23fcac4
KC <kochihuang202@github.com>
2026-06-19 23:39:06 +0800

    chore: bump deploy version to bust cache for series integration

--- cloudflare/worker-proxy.js
+++ cloudflare/worker-proxy.js
@@ -24,7 +24,7 @@ function resolveAssetPath(pathname) {
   return pathname;
 }
 
-const DEPLOY_VERSION = "v_2026_06_19_23_35";
+const DEPLOY_VERSION = "v_2026_06_19_23_39";
 
 async function fetchAsset(pathname) {
   const assetPath = resolveAssetPath(pathname);
