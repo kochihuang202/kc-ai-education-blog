9fe4e83b600fa3d9a0781482d8d0b06b65ada04d
KC <kochihuang202@github.com>
2026-06-19 23:34:45 +0800

    style: shrink book image display size to half

--- cloudflare/worker-proxy.js
+++ cloudflare/worker-proxy.js
@@ -24,7 +24,7 @@ function resolveAssetPath(pathname) {
   return pathname;
 }
 
-const DEPLOY_VERSION = "v_2026_06_19_23_33";
+const DEPLOY_VERSION = "v_2026_06_19_23_35";
 
 async function fetchAsset(pathname) {
   const assetPath = resolveAssetPath(pathname);
