1c069b614a58147162e28f101b738bf65113e831
KC <kochihuang202@github.com>
2026-08-02 20:34:20 +0800

    style: enforce clean sans-serif font across all articles

--- cloudflare/worker-proxy.js
+++ cloudflare/worker-proxy.js
@@ -24,7 +24,7 @@ function resolveAssetPath(pathname) {
   return pathname;
 }
 
-const DEPLOY_VERSION = "v_2026_08_02_20_28";
+const DEPLOY_VERSION = "v_2026_08_02_20_34";
 
 async function fetchAsset(pathname) {
   const assetPath = resolveAssetPath(pathname);
