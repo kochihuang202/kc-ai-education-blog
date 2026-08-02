ae4e75739b5a8c02162a1360f7fea266d81eeea9
KC <kochihuang202@github.com>
2026-08-02 20:49:07 +0800

    fix: full width card media with top cover positioning

--- cloudflare/worker-proxy.js
+++ cloudflare/worker-proxy.js
@@ -24,7 +24,7 @@ function resolveAssetPath(pathname) {
   return pathname;
 }
 
-const DEPLOY_VERSION = "v_2026_08_02_20_43";
+const DEPLOY_VERSION = "v_2026_08_02_20_48";
 
 async function fetchAsset(pathname) {
   const assetPath = resolveAssetPath(pathname);
